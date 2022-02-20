ad2693f39d1b2de84e00ea5428a10697acb752b6
AsaTi <pommermann@hotmail.de>
2022-02-20 10:29:25 +0100

    ++

--- Assets/AAAProjectAssets/Scripts/Manager/GameOver.cs
+++ Assets/AAAProjectAssets/Scripts/Manager/GameOver.cs
@@ -1,11 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using MAED.ActionAndStates;
 
 public class GameOver : MonoBehaviour
 {
+    private UIManager uiManager;
+
+    private PlugableStateController player;
+
+    private bool isGameovered;
+
+    private void Start()
+    {
+        uiManager = GetComponent<UIManager>();
+        player = GetComponent<AbilityManager>().PlayerController;
+    }
+
     public void TimerOver()
     {
-        Debug.LogError("RESET TO START??");
+        if (isGameovered)
+            return;
+        isGameovered = true;
+        uiManager.ChangeText("You ran out of Time");
+
+        StartCoroutine(TitleReturnTimer());
     }
+    public void DeathTouchOver()
+    {
+        if (isGameovered)
+            return;
+        isGameovered = true;
+
+        uiManager.ChangeText("There is no escape from Death");
+
+        StartCoroutine(TitleReturnTimer());
+    }
+
+    private IEnumerator TitleReturnTimer()
+    {
+        player.IsActive = false;
+
+        yield return new WaitForSeconds(5f);
+
+        SceneManager.LoadScene(0);
+    }
+
 }
