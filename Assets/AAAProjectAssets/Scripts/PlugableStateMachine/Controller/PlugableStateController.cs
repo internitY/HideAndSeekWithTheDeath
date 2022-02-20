69e4c7cd4a3c0af2f1ff0380e0398eb4b1435825
AsaTi <pommermann@hotmail.de>
2022-02-20 10:51:29 +0100

    GameOver by Touch + Update Titlescreen

--- Assets/AAAProjectAssets/Scripts/PlugableStateMachine/Controller/PlugableStateController.cs
+++ Assets/AAAProjectAssets/Scripts/PlugableStateMachine/Controller/PlugableStateController.cs
@@ -299,6 +299,10 @@ namespace MAED.ActionAndStates
         public void TakeDamage(PlugableStateController attacker)
         {
             Debug.Log(name + " get attacked by " + attacker.name);
+            if (gameObject.CompareTag("Player"))
+            {
+                CollectableManager.Instance.GetComponent<GameOver>().DeathTouchOver();
+            }
         }
         #endregion target chasing
 
